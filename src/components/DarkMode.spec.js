b9e7df7861fdc03df57dcdb9f57d19c65f704d2f
Kris Kelly <bertpotato@gmail.com>
2020-02-15 02:26:49 +0000

    * tidied up DarkMode.spec.js

--- src/components/DarkMode.spec.js
+++ src/components/DarkMode.spec.js
@@ -27,7 +27,6 @@ test( '<DarkMode enabled={true} />', () => {
 test( '<DarkMode is clickable and toggles mode />', async () => {
     const wrapper = render( DarkMode, { props: { enabled: false } } )
 
-    // wrapper.debug()
 
     let button = wrapper.getByTestId( 'darkmode-button' )
 
@@ -35,7 +34,9 @@ test( '<DarkMode is clickable and toggles mode />', async () => {
     expect( button.textContent ).toBe( '[DM]' )
 
     await fireEvent.click( button );
+    // wrapper.debug()
 
+    expect( wrapper.container.classList.contains('dark-mode')).toBe(true)
     expect( button.textContent ).toBe( '[LM]' )
     expect( button.title ).toBe( 'Toggle dark mode off' )
 } )
@@ -47,51 +48,6 @@ test( '<DarkMode is accessible />', async () => {
 
     let button = wrapper.getByTestId( 'darkmode-button' )
 
+    expect( button.title ).toBe( 'Toggle dark mode on' )
     expect( button.getAttribute('role') ).toBe( 'switch' )
 } )
-//
-// test('<Counter /> destructuring and getting test by ID', () => {
-//   const {debug, getByTestId } = render(Counter);
-//   //* if we want to debug
-//   debug();
-//   //* Assertions with destructuring
-//   const button = getByTestId('counter-button');
-//   expect(button.tagName).toBe('BUTTON');
-//   expect(button.textContent).toBe('0');
-// })
-//
-//
-// //* just when we test actions, we need to do it with async/await
-// test('<Counter /> fireEvent', async () => {
-//   const { getByTestId, debug, getByText } = render(Counter);
-//
-//   //* This can be used as "The component is rendered"
-//   const counterButton = getByTestId('counter-button');
-//   expect(counterButton.textContent).toBe('0');
-//
-//   //* Another way to do it, retrieving the DOM printed if is failing.
-//   expect(getByTestId('counter-button')).toBeTruthy();
-//
-//   //* If we click once
-//   await fireEvent.click(counterButton);
-//   expect(counterButton.textContent).toBe('1');
-//
-//   //* If we click twice and so on
-//   await fireEvent.click(counterButton);
-//   expect(counterButton.textContent).toBe('2');
-//
-//   //* Another approach
-//   await fireEvent.click(counterButton);
-//   const button = getByText('3'); // we can not retrieve a funcion like in React
-//   expect(button).toBeTruthy();
-//
-//   //* Another option but too verbose
-//   await fireEvent(
-//     counterButton,
-//     new MouseEvent('click', {
-//       bubbles: true,
-//       cancelable: true,
-//     })
-//   )
-//   expect(button).toHaveTextContent('4')
-// });
