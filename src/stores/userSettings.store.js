d5faa9494ef3d92e623ceb59444b121ab84cbee1
Kris <bertpotato@gmail.com>
2020-07-21 16:07:04 +0100

    * semi-colon

--- src/stores/userSettings.store.js
+++ src/stores/userSettings.store.js
@@ -1,7 +1,7 @@
 import { writable } from 'svelte-persistent-store/dist/local'
 
 // increment each time something changes to help migration of future settings
-export const SETTINGS_VERSION = 2;
+export const SETTINGS_VERSION = 2
 /**
  *
  * @type {{pattern: string | string, numberToGenerate: number, darkMode: boolean, version: number}}
@@ -16,13 +16,13 @@ const defaultSettings = {
 const userSettings = writable( 'user_settings', JSON.stringify( defaultSettings ) )
 
 export function getUserSettings() {
-    let result = {};
-    let versionNumber = 0;
+    let result = {}
+    let versionNumber = 0
     let unsubscribe = userSettings.subscribe( settings => {
         if (settings) {
             result = JSON.parse( settings )
             if (result.version) {
-                versionNumber = parseInt(result.version, 10) // for future use
+                versionNumber = parseInt( result.version, 10 ) // for future use
             }
         }
     } )
@@ -39,26 +39,26 @@ export function getUserSettings() {
  * @returns {{pattern: string | string, numberToGenerate: number, darkMode: boolean, version: number}}
  */
 function upgradeSettings(settings) {
-    let defaultSettingsKeys = Object.getOwnPropertyNames(defaultSettings);
+    let defaultSettingsKeys = Object.getOwnPropertyNames( defaultSettings )
 
-    defaultSettingsKeys.forEach((property) => {
-        if (typeof settings[property] === 'undefined'){
-            settings[property] = defaultSettings[property];
-        }
-    })
-
-    //strip unknown properties out
-    Object.keys(settings).forEach( (property) => {
-        if (typeof defaultSettings[property] === 'undefined') {
-            delete settings[property];
+    defaultSettingsKeys.forEach( (property) => {
+        if (typeof settings[property] === 'undefined') {
+            settings[property] = defaultSettings[property]
         }
     } )
-    settings.version = defaultSettings.version;
-    return settings;
+
+    //strip unknown properties out
+    Object.keys( settings ).forEach( (property) => {
+        if (typeof defaultSettings[property] === 'undefined') {
+            delete settings[property]
+        }
+    } )
+    settings.version = defaultSettings.version
+    return settings
 }
 
 export function setUserSettings(settings) {
-    settings = upgradeSettings(settings);
+    settings = upgradeSettings( settings )
     settings = settings || defaultSettings
 
     userSettings.set( JSON.stringify( settings ) )
